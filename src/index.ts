c85edeabd4634ec150c11cbcf5329b9c0be4a847
Francesco Grauso <grausof@gmail.com>
2023-07-25 15:59:18 +0200

    chore: Export errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as PID from "./pid";
 import * as RP from "./rp";
+import * as Errors from "./utils/errors";
 import * as WalletInstanceAttestation from "./wallet-instance-attestation";
 import { getUnsignedDPop } from "./utils/dpop";
 import { getSignedJwt } from "./utils/signature";
 
-export { PID, RP, WalletInstanceAttestation, getUnsignedDPop, getSignedJwt };
+export {
+  PID,
+  RP,
+  WalletInstanceAttestation,
+  Errors,
+  getUnsignedDPop,
+  getSignedJwt,
+};
