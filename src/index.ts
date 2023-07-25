b4523f0ebb724b65bf5ebfbaf18c4b72eda2acb0
grausof <grausof@gmail.com>
2023-07-25 13:28:53 +0200

    Export errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as PID from "./pid";
 import * as WalletInstanceAttestation from "./wallet-instance-attestation";
+import * as Errors from "./utils/errors";
 
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
-export { PID, WalletInstanceAttestation };
+export { PID, WalletInstanceAttestation, Errors };
