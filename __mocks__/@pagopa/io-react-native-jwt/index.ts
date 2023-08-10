0dd86d0d8eb95be2f006d29e0057d5344e24eb85
Francesco Grauso <grausof@gmail.com>
2023-08-10 11:52:32 +0200

    fix: Fix some jest mock problem
    Refs: #27

--- __mocks__/@pagopa/io-react-native-jwt/index.ts
+++ __mocks__/@pagopa/io-react-native-jwt/index.ts
@@ -1,18 +1,16 @@
-// mock react native bridged modules
+import { sha256 as mockSha256 } from "js-sha256";
+import type { JWK } from "src/utils/jwk";
 
-import { sha256 } from "js-sha256";
-export * from "@pagopa/io-react-native-jwt";
-export const sha256ToBase64 = (v: string) =>
-  removePadding(hexToBase64(sha256(v)));
+export const sha256ToBase64 = async (toHash: string) =>
+  Promise.resolve(removePadding(mockHexToBase64(mockSha256(toHash))));
 
-function hexToBase64(hexstring: string) {
+function mockHexToBase64(hexstring: string) {
   const x = hexstring.match(/\w{2}/g) || [];
   const g = x
     .map(function (a) {
       return String.fromCharCode(parseInt(a, 16));
     })
     .join("");
-
   return btoa(g);
 }
 
@@ -20,3 +18,14 @@ function removePadding(encoded: string): string {
   // eslint-disable-next-line no-div-regex
   return encoded.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
 }
+
+export const decode = (jwt: string) => {
+  const [encodedHeader, encodedPayload, _signature] = jwt.split(".");
+  const payload = JSON.parse(atob(encodedPayload as string));
+  const protectedHeader = JSON.parse(atob(encodedHeader as string));
+  return { payload, protectedHeader };
+};
+export const verify = (jwt: string, _: JWK) => decode(jwt);
+
+export const decodeBase64 = (value: string) => atob(value);
+export const encodeBase64 = (value: string) => removePadding(btoa(value));
--- __mocks__/react-native-uuid/index.ts
+++
@@ -1,3 +0,0 @@
-import { randomUUID } from "crypto";
-export * from "react-native-uuid";
-export const v4 = () => randomUUID();
