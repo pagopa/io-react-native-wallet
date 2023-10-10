f8d7678ea16620fabd360d2ba380aa62ee829028
Emanuele De Cupis <ema.decu@gmail.com>
2023-10-10 15:40:01 +0200

    Small syntax fix (#57)

--- src/utils/__tests__/crypto.test.ts
+++ src/utils/__tests__/crypto.test.ts
@@ -27,4 +27,22 @@ describe("useEphemeralKey", () => {
     expect(generate).toBeCalledTimes(1);
     expect(deleteKey).toBeCalledTimes(1);
   });
+
+  it("should delete only after the procedure has ended", async () => {
+    let a: number = 0,
+      b: number = -1;
+    const fn = () =>
+      new Promise((ok) => setTimeout((_) => ok((a = Date.now())), 1000));
+
+    (deleteKey as jest.Mock).mockImplementationOnce(
+      (_) => new Promise((ok) => setTimeout(() => ok((b = Date.now())), 100))
+    );
+
+    await useEphemeralKey(fn);
+
+    expect(generate).toBeCalledTimes(1);
+    expect(deleteKey).toBeCalledTimes(1);
+
+    expect(a).toBeLessThan(b);
+  });
 });
