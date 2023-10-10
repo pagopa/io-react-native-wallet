cf7a00e7de7e0ddc59b7615d26df99c6e52b3123
Emanuele De Cupis <ema.decu@gmail.com>
2023-10-10 15:06:29 +0200

    fix

--- src/utils/__tests__/crypto.test.ts
+++ src/utils/__tests__/crypto.test.ts
@@ -27,4 +27,21 @@ describe("useEphemeralKey", () => {
     expect(generate).toBeCalledTimes(1);
     expect(deleteKey).toBeCalledTimes(1);
   });
+
+  it("should delete only after the procedure has ended", async () => {
+    let a: number, b: number;
+    const fn = () =>
+      new Promise((ok) => setTimeout((_) => ok((a = Date.now())), 1000));
+
+    (deleteKey as jest.Mock).mockImplementationOnce(
+      (_) => new Promise((ok) => setTimeout(() => ok((b = Date.now())), 100))
+    );
+
+    await useEphemeralKey(fn);
+
+    expect(generate).toBeCalledTimes(1);
+    expect(deleteKey).toBeCalledTimes(1);
+
+    expect(a).toBeLessThan(b);
+  });
 });
