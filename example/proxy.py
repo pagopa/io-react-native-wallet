f9d5598423d9b2b5ebd2621bf0f4d2d3fd1faba4
grausof <grausof@gmail.com>
2024-05-15 10:09:32 +0200

    Update proxy.py

--- example/proxy.py
+++ example/proxy.py
@@ -10,16 +10,23 @@ import requests  # pip package requests
 
 app = Flask(__name__)
 
-API_HOST = "https://walletprovider.example.com"
-USER_ID = "User UUID here"
+API_HOST = ""
+USER_ID = ""
+APP_KEY = ""
 
 @app.route('/', defaults={'path': ''}, methods=["GET", "POST", "PUT"])
 @app.route('/<path>', methods=["GET", "POST", "PUT"])
 def redirect_to_API_HOST(path):  #NOTE var :path will be unused as all path we need will be read from :request ie from flask import request
+
+    # exclude 'host' header
+    request_headers = {k:v for k,v in request.headers if k.lower() != 'host'}
+    request_headers["x-iowallet-user-id"] = USER_ID
+    request_headers["x-functions-key"] = APP_KEY
+
     res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
         method          = request.method,
         url             = request.url.replace(request.host_url, f'{API_HOST}/'),
-        headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
+        headers         = request_headers,
         data            = request.get_data(),
         cookies         = request.cookies,
         allow_redirects = False,
@@ -31,8 +38,6 @@ def redirect_to_API_HOST(path):  #NOTE var :path will be unused as all path we n
         (k,v) for k,v in res.raw.headers.items()
         if k.lower() not in excluded_headers
     ]
-
-    headers['x-iowallet-user-id'] = USER_ID
 
     response = Response(res.content, res.status_code, headers)
     return response
