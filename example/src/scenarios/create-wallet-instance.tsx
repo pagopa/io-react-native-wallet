f756ba7a80afea975a1fcada11ad064f91908700
LazyAfternoons <lazyafternoons@outlook.it>
2024-05-23 10:20:50 +0200

    feat: add android example app

--- example/src/scenarios/create-wallet-instance.tsx
+++ example/src/scenarios/create-wallet-instance.tsx
@@ -7,13 +7,56 @@ import {
   generateHardwareKey,
   getAttestation,
   isAttestationServiceAvailable,
+  isPlayServicesAvailable,
+  prepareIntegrityToken,
 } from "@pagopa/io-react-native-integrity";
 
-import { WALLET_PROVIDER_BASE_URL } from "@env";
+import { WALLET_PROVIDER_BASE_URL, GOOGLE_CLOUD_PROJECT_NUMBER } from "@env";
+import { generate, type CryptoError } from "@pagopa/io-react-native-crypto";
+import { Platform } from "react-native";
 
 const walletProviderBaseUrl = WALLET_PROVIDER_BASE_URL;
 
-export default async () => {
+export default async () =>
+  Platform.select({
+    ios: () => createIos(),
+    android: () => createAndroid(),
+    default: () => Promise.reject(new Error("Unsupported platform")),
+  })();
+
+const createAndroid = async () => {
+  try {
+    const isPlayServiceAvailable = await isPlayServicesAvailable();
+    if (!isPlayServiceAvailable) {
+      throw new Error("Play services unavailable");
+    }
+
+    const keyTag = `${Math.random()}`;
+
+    await generate(keyTag).catch((e) => {
+      if ((e as CryptoError).message !== "KEY_ALREADY_EXISTS") {
+        throw e;
+      }
+    });
+
+    const integrityContext: IntegrityContext = {
+      getHardwareKeyTag: () => keyTag,
+      getAttestation: (nonce) => getAttestation(nonce, keyTag),
+    };
+
+    const createdWalletInstance = await WalletInstance.createWalletInstance({
+      integrityContext,
+      walletProviderBaseUrl,
+    });
+    console.log(createdWalletInstance);
+    return result(createdWalletInstance);
+  } catch (e) {
+    console.error(e);
+    return error(e);
+  }
+};
+
+const createIos = async () => {
   try {
     const isIntegrityAvailable = await isAttestationServiceAvailable();
     if (!isIntegrityAvailable) {
