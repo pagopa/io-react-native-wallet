f7bf6f81dff8975ca266b5cc5f640d4a1ae8649c
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2024-05-23 15:06:29 +0200

    chore: Add Android example app (#84)
    Refs: SIW-1160

--- example/src/scenarios/create-wallet-instance.tsx
+++ example/src/scenarios/create-wallet-instance.tsx
@@ -10,17 +10,18 @@ import {
 } from "@pagopa/io-react-native-integrity";
 
 import { WALLET_PROVIDER_BASE_URL } from "@env";
+import { generate } from "@pagopa/io-react-native-crypto";
+import { Platform } from "react-native";
 
 const walletProviderBaseUrl = WALLET_PROVIDER_BASE_URL;
 
 export default async () => {
   try {
-    const isIntegrityAvailable = await isAttestationServiceAvailable();
-    if (!isIntegrityAvailable) {
-      throw new Error("Attestation service unavailable");
-    }
-
-    const hardwareKeyTag = await generateHardwareKey();
+    const hardwareKeyTag = await Platform.select({
+      ios: () => generateKeyIos(),
+      android: () => generateKeyAndroid(),
+      default: () => Promise.reject(new Error("Unsupported platform")),
+    })();
 
     const integrityContext: IntegrityContext = {
       getHardwareKeyTag: () => hardwareKeyTag,
@@ -38,4 +39,30 @@ export default async () => {
     console.error(e);
     return error(e);
   }
+};
+
+/**
+ * Generates the hardware backed key for iOS.
+ * It differents from Android because the key is generated via `io-react-native-integrity` instead of `io-react-native-crypto`.
+ * There's also a check for the availability of the attestation service.
+ * @returns a promise that resolves with the key tag as string.
+ */
+const generateKeyIos = async () => {
+  const isIntegrityAvailable = await isAttestationServiceAvailable();
+  if (!isIntegrityAvailable) {
+    throw new Error("Attestation service unavailable");
+  }
+
+  return await generateHardwareKey();
+};
+
+/**
+ * Generates the hardware backed key for Android.
+ * It differents from iOS because the key is generated via `io-react-native-crypto` instead of `io-react-native-integrity`.
+ * @returns a promise that resolves with the key tag as string.
+ */
+const generateKeyAndroid = async () => {
+  const keyTag = `${Math.random()}`;
+  await generate(keyTag);
+  return keyTag;
 };
