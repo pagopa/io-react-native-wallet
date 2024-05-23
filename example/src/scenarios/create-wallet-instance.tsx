306bd22d9dc2c8ac5b389e0fa6fe43329c5f82c7
LazyAfternoons <lazyafternoons@outlook.it>
2024-05-23 10:43:46 +0200

    chore: remove duplicate code

--- example/src/scenarios/create-wallet-instance.tsx
+++ example/src/scenarios/create-wallet-instance.tsx
@@ -7,62 +7,21 @@ import {
   generateHardwareKey,
   getAttestation,
   isAttestationServiceAvailable,
-  isPlayServicesAvailable,
 } from "@pagopa/io-react-native-integrity";
 
 import { WALLET_PROVIDER_BASE_URL } from "@env";
-import { generate, type CryptoError } from "@pagopa/io-react-native-crypto";
+import { generate } from "@pagopa/io-react-native-crypto";
 import { Platform } from "react-native";
 
 const walletProviderBaseUrl = WALLET_PROVIDER_BASE_URL;
 
-export default async () =>
-  Platform.select({
-    ios: () => createIos(),
-    android: () => createAndroid(),
-    default: () => Promise.reject(new Error("Unsupported platform")),
-  })();
-
-const createAndroid = async () => {
+export default async () => {
   try {
-    const isPlayServiceAvailable = await isPlayServicesAvailable();
-    if (!isPlayServiceAvailable) {
-      throw new Error("Play services unavailable");
-    }
-
-    const keyTag = `${Math.random()}`;
-
-    await generate(keyTag).catch((e) => {
-      if ((e as CryptoError).message !== "KEY_ALREADY_EXISTS") {
-        throw e;
-      }
-    });
-
-    const integrityContext: IntegrityContext = {
-      getHardwareKeyTag: () => keyTag,
-      getAttestation: (nonce) => getAttestation(nonce, keyTag),
-    };
-
-    const createdWalletInstance = await WalletInstance.createWalletInstance({
-      integrityContext,
-      walletProviderBaseUrl,
-    });
-    console.log(createdWalletInstance);
-    return result(createdWalletInstance);
-  } catch (e) {
-    console.error(e);
-    return error(e);
-  }
-};
-
-const createIos = async () => {
-  try {
-    const isIntegrityAvailable = await isAttestationServiceAvailable();
-    if (!isIntegrityAvailable) {
-      throw new Error("Attestation service unavailable");
-    }
-
-    const hardwareKeyTag = await generateHardwareKey();
+    const hardwareKeyTag = await Platform.select({
+      ios: () => generateKeyIos(),
+      android: () => generateKeyAndroid(),
+      default: () => Promise.reject(new Error("Unsupported platform")),
+    })();
 
     const integrityContext: IntegrityContext = {
       getHardwareKeyTag: () => hardwareKeyTag,
@@ -80,4 +39,30 @@ const createIos = async () => {
     console.error(e);
     return error(e);
   }
+};
+
+/**
+ * Generates the hardware backed key for iOS.
+ * It differents from Android because the key is generated via `io-react-native-integrity` instead of `io-react-native-crypto`.
+ * There's also a check for the availability of the attestation service.
+ * @returns a promise that resolves with the key tag as string.
+ */
+const generateKeyIos = async () => {
+  const isIntegrityAvailable = await isAttestationServiceAvailable();
+  if (!isIntegrityAvailable) {
+    throw new Error("Attestation service unavailable");
+  }
+
+  return await generateHardwareKey();
+};
+
+/**
+ * Generates the hardware backed key for Android.
+ * It differents from iOS because the key is generated via `io-react-native-crypto` instead of `io-react-native-integrity`.
+ * @returns a promise that resolves with the key tag as string.
+ */
+const generateKeyAndroid = async () => {
+  const keyTag = `${Math.random()}`;
+  await generate(keyTag);
+  return keyTag;
 };
