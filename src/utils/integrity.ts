b4316bb9fbb77882e13490c485de3d9dfa3efd5d
LazyAfternoons <lazyafternoons@outlook.it>
2024-05-28 15:16:37 +0200

    chore: adjustments

--- src/utils/integrity.ts
+++ src/utils/integrity.ts
@@ -1,9 +1,5 @@
-import type { PublicKey } from "@pagopa/io-react-native-crypto";
-import type { JWK } from "./jwk";
-
 export interface IntegrityContext {
   getHardwareKeyTag: () => string;
-  getHardwarePublicKey: () => Promise<JWK>;
   getAttestation: (nonce: string) => Promise<string>;
   getHardwareSignatureWithAuthData: (
     clientData: string
