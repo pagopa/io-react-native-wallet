49063338354b96350ca0651d6838a6ae915e0cc0
LazyAfternoons <lazyafternoons@outlook.it>
2024-05-28 15:46:33 +0200

    chore: remove unused styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -39,17 +39,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     margin: 16,
   },
-  title: {
-    textAlign: "center",
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
 });
