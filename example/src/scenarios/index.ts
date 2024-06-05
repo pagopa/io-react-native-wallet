b1fad9b80b91ee63d412eff116cdfdc4a0b72154
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2024-06-05 16:41:23 +0200

    chore: [SIW-1202] Remove poc scenarios (#87)

--- example/src/scenarios/index.ts
+++ example/src/scenarios/index.ts
@@ -1,34 +1,8 @@
-import getEntityStatement from "./poc/get-entity-statement";
-import getPid from "./poc/get-pid";
-import getAttestation from "./poc/get-attestation";
-import verifyPid from "./poc/verify-pid";
-import decodePid from "./poc/decode-pid";
-import decodeCredentialSdJwt from "./poc/decode-credential-sdjwt";
-import verifyCredentialSdJwt from "./poc/verify-credential-sdjwt";
-import decodeQR from "./poc/decode-qr-from-rp";
-import authenticationToRP from "./poc/cross-device-flow-with-rp";
-import getCredential from "./poc/get-credential";
-import getMultipleCredential from "./poc/get-credential-multiple";
 import createWalletInstance from "./production/create-wallet-instance";
 import TestScenario from "./TestScenario";
 
 const scenarios = {
-  poc: {
-    getPid,
-    getAttestation,
-    getCredential,
-    getMultipleCredential,
-    verifyPid,
-    decodePid,
-    decodeQR,
-    authenticationToRP,
-    getEntityStatement,
-    decodeCredentialSdJwt,
-    verifyCredentialSdJwt,
-  },
-  prod: {
-    createWalletInstance,
-  },
+  createWalletInstance,
 };
 
 export default scenarios;
