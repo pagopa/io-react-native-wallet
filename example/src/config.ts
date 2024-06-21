efcdac8a897149909cf054d06c82a8f7e322f5fe
Francesco Grauso <grausof@gmail.com>
2024-06-21 10:49:52 +0200

    fix: [SIW-1273] Fix dev-proxy and rename env variables (#92)

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,6 +1,6 @@
 declare module "@env" {
   export const WALLET_PROVIDER_BASE_URL: string;
   export const GOOGLE_CLOUD_PROJECT_NUMBER: string;
-  export const ISSUER_BASE_URL: string;
-  export const ISSUER_AUTH_TOKEN: string;
+  export const WALLET_PID_PROVIDER_BASE_URL: string;
+  export const WALLET_PID_PROVIDER_AUTH_TOKEN: string;
 }
