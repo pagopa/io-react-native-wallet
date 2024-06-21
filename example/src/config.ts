f531baf8c27d9aa4ac7fc0cc14f21a1d3c4c5d92
grausof <grausof@gmail.com>
2024-06-21 09:35:52 +0200

    Fix .env for PID_PROVIDER

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,9 +1,8 @@
 declare module "@env" {
   export const WALLET_PROVIDER_BASE_URL: string;
   export const WALLET_PID_PROVIDER_BASE_URL: string;
+  export const WALLET_PID_PROVIDER_AUTH_TOKEN: string;
   export const WALLET_PID_PROVIDER_REDIRECT_OVERRIDE_URL: string;
   export const REDIRECT_URI: string;
   export const GOOGLE_CLOUD_PROJECT_NUMBER: string;
-  export const ISSUER_BASE_URL: string;
-  export const ISSUER_AUTH_TOKEN: string;
 }
