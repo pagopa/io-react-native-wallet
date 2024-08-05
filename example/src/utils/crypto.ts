98dda640e8dac208b1eb0888b5843c1fad64fb06
LazyAfternoons <lazyafternoons@outlook.it>
2024-08-05 17:29:48 +0200

    docs: update documentation

--- example/src/utils/crypto.ts
+++ example/src/utils/crypto.ts
@@ -4,12 +4,22 @@ import {
   type CryptoError,
 } from "@pagopa/io-react-native-crypto";
 
+/**
+ * Regenerates a crypto key by deleting it if it exists and then generating a new one.
+ * This function simply tries to delete it without checking if it exists first, ignoring the specific exception thrown in this case and then generates a new one.
+ * @param keyTag - The key tag to be associated with the crypto key
+ */
 export const regenerateCryptoKey = async (keyTag: string) => {
   // Delete the key if it exists, otherwise ignore the error
   await deleteKeyIfExists(keyTag);
   await generate(keyTag);
 };
 
+/**
+ * Deletes a crypto key without checking if it exists first, ignoring the specific exception thrown in this case.
+ * It shadows the error thrown when the key is not found.
+ * @param keyTag - The key tag to be associated with the crypto key
+ */
 export const deleteKeyIfExists = async (keyTag: string) => {
   await deleteKey(keyTag).catch((e) => {
     const { message } = e as CryptoError;
