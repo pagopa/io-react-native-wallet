2e98766cfe0b1fd0244759ac6786e3e6ad841a42
Riccardo.Molinari <riccardo.molinari@it.ey.com>
2024-11-08 09:36:02 +0100

    fix: lint

--- example/src/utils/misc.ts
+++ example/src/utils/misc.ts
@@ -10,45 +10,45 @@
  * @returns A promise that resolves once the conditionFunction returns true or rejects if timed out.
  */
 export const until = (
-    conditionFunction: () => boolean,
-    timeoutSeconds?: number
-  ): Promise<void> =>
-    new Promise<void>((resolve, reject) => {
-      const start = Date.now();
-      const poll = () => {
-        if (conditionFunction()) {
-          resolve();
-        } else if (
-          timeoutSeconds !== undefined &&
-          Date.now() - start >= timeoutSeconds * 1000
-        ) {
-          reject(new Error("Timeout exceeded"));
-        } else {
-          setTimeout(poll, 400);
-        }
-      };
-  
-      poll();
-    });
+  conditionFunction: () => boolean,
+  timeoutSeconds?: number
+): Promise<void> =>
+  new Promise<void>((resolve, reject) => {
+    const start = Date.now();
+    const poll = () => {
+      if (conditionFunction()) {
+        resolve();
+      } else if (
+        timeoutSeconds !== undefined &&
+        Date.now() - start >= timeoutSeconds * 1000
+      ) {
+        reject(new Error("Timeout exceeded"));
+      } else {
+        setTimeout(poll, 400);
+      }
+    };
+
+    poll();
+  });
 
 /**
  * Creates a promise that waits until the provided signal is aborted.
  * @returns {Object} An object with `listen` and `remove` methods to handle subscribing and unsubscribing.
  */
 export const createAbortPromiseFromSignal = (signal: AbortSignal) => {
-    let listener: () => void;
-    return {
-      listen: () =>
-        new Promise<"OPERATION_ABORTED">((resolve) => {
-          if (signal.aborted) {
-            return resolve("OPERATION_ABORTED");
-          }
-          listener = () => resolve("OPERATION_ABORTED");
-          signal.addEventListener("abort", listener);
-        }),
-      remove: () => signal.removeEventListener("abort", listener),
-    };
+  let listener: () => void;
+  return {
+    listen: () =>
+      new Promise<"OPERATION_ABORTED">((resolve) => {
+        if (signal.aborted) {
+          return resolve("OPERATION_ABORTED");
+        }
+        listener = () => resolve("OPERATION_ABORTED");
+        signal.addEventListener("abort", listener);
+      }),
+    remove: () => signal.removeEventListener("abort", listener),
   };
-  
-  export const isDefined = <T>(x: T | undefined | null | ""): x is T =>
-    Boolean(x);
+};
+
+export const isDefined = <T>(x: T | undefined | null | ""): x is T =>
+  Boolean(x);
