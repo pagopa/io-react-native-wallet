dd50623dc1feffba13ef9c285ac9437ea00c74ce
LazyAfternoons <lazyafternoons@outlook.it>
2025-01-15 11:37:20 +0100

    chore: remove status attestation for credential

--- src/credential/index.ts
+++ src/credential/index.ts
@@ -1,6 +1,5 @@
 import * as Issuance from "./issuance";
 // import * as Presentation from "./presentation";
-import * as Status from "./status";
 import * as Trustmark from "./trustmark";
 
-export { Issuance, Status, Trustmark };
+export { Issuance, Trustmark };
