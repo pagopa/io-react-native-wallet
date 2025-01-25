bd98f179655ee294bf433ab1570fb0538e445117
LazyAfternoons <lazyafternoons@outlook.it>
2025-01-25 10:53:41 +0100

    chore: remove eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,4 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: ["@react-native", "prettier"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        tabWidth: 2,
-        trailingComma: "es5",
-        useTabs: false,
-      },
-    ],
-  },
 };
