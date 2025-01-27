9c0f44145b87c928988926b02bd8adeff13510be
LazyAfternoons <lazyafternoons@outlook.it>
2025-01-27 12:08:53 +0100

    fix: attempt at fixing statics

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
   root: true,
-  extends: ["@react-native"],
-  plugins: ["@react-native"],
+  extends: ["@react-native", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: "es5",
+        useTabs: false,
+      },
+    ],
+  },
 };
