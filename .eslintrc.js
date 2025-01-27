f0ef1a7ffeeba019134e1d6596796ef7040f60b3
LazyAfternoons <lazyafternoons@outlook.it>
2025-01-27 12:30:50 +0100

    fix: attempt at fixing statics

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: ["@react-native", "prettier"],
-  plugins: ["prettier", "jest"],
+  plugins: ["prettier"],
   rules: {
     "prettier/prettier": [
       "error",
