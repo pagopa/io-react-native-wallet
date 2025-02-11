a0937ad319bffbf6fb0309a7fed9174a7f1cfd89
Gianluca Spada <gianluca.spada@spindox.it>
2025-02-11 16:40:54 +0100

    chore(presentation): move QR code validation on the consumer side

--- src/credential/presentation/01-start-flow.ts
+++ src/credential/presentation/01-start-flow.ts
@@ -1,23 +1,22 @@
 import * as z from "zod";
-import { InvalidQRCodeError } from "./errors";
+import { ValidationFailed } from "../../utils/errors";
 
-const QRCodePayload = z.object({
-  protocol: z.string(),
-  resource: z.string(), // TODO: refine to known paths using literals
+const PresentationParams = z.object({
   clientId: z.string(),
   requestURI: z.string(),
   requestURIMethod: z.enum(["get", "post"]),
   state: z.string().optional(),
 });
+type PresentationParams = z.infer<typeof PresentationParams>;
 
 /**
  * The beginning of the presentation flow.
  * To be implemented accordind to the user touchpoint
  *
- * @param Optional parameters, depending on the starting touchoint
+ * @param params Presentation parameters, depending on the starting touchoint
  * @returns The url for the Relying Party to connect with
  */
-export type StartFlow<T extends Array<unknown> = []> = (...args: T) => {
+export type StartFlow = (params: PresentationParams) => {
   requestURI: string;
   clientId: string;
   requestURIMethod?: "get" | "post";
@@ -27,44 +26,19 @@ export type StartFlow<T extends Array<unknown> = []> = (...args: T) => {
 /**
  * Start a presentation flow by decoding an incoming QR-code
  *
- * @param qrcode The encoded QR-code content
+ * @param params The encoded QR-code content
  * @returns The url for the Relying Party to connect with
  * @throws If the provided qr code fails to be decoded
  */
-export const startFlowFromQR: StartFlow<[string]> = (qrcode) => {
-  let decodedUrl: URL;
-  try {
-    // splitting qrcode to identify which is link format
-    const originalQrCode = qrcode.split("://");
-    const replacedQrcode = originalQrCode[1]?.startsWith("?")
-      ? qrcode.replace(`${originalQrCode[0]}://`, "https://wallet.example/")
-      : qrcode;
-
-    decodedUrl = new URL(replacedQrcode);
-  } catch (error) {
-    throw new InvalidQRCodeError(`Failed to decode QR code:  ${qrcode}`);
-  }
-
-  const protocol = decodedUrl.protocol;
-  const resource = decodedUrl.hostname;
-  const requestURI = decodedUrl.searchParams.get("request_uri");
-  const requestURIMethod =
-    decodedUrl.searchParams.get("request_uri_method") ?? "get";
-  const clientId = decodedUrl.searchParams.get("client_id");
-  const state = decodedUrl.searchParams.get("state") ?? undefined;
-
-  const result = QRCodePayload.safeParse({
-    protocol,
-    resource,
-    requestURI,
-    requestURIMethod,
-    clientId,
-    state,
-  });
+export const startFlowFromQR: StartFlow = (params) => {
+  const result = PresentationParams.safeParse(params);
 
   if (result.success) {
     return result.data;
   } else {
-    throw new InvalidQRCodeError(`${result.error.message}, ${decodedUrl}`);
+    throw new ValidationFailed({
+      message: "Invalid parameters provided",
+      reason: result.error.message,
+    });
   }
 };
