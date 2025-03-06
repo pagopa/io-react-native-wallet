6f8efcee599ae713f7037f9966f1818087b3ce90
Gianluca Spada <gianluca.spada@spindox.it>
2025-03-06 15:32:21 +0100

    chore: remove function to get JWKS from Request Object

--- src/credential/presentation/04-retrieve-rp-jwks.ts
+++ src/credential/presentation/04-retrieve-rp-jwks.ts
@@ -1,8 +1,5 @@
-import { JWKS, JWK } from "../../utils/jwk";
-import { hasStatusOrThrow } from "../../utils/misc";
+import { JWK } from "../../utils/jwk";
 import { RelyingPartyEntityConfiguration } from "../../trust/types";
-import { decode as decodeJwt } from "@pagopa/io-react-native-jwt";
-import { NoSuitableKeysFoundInEntityConfiguration } from "./errors";
 
 /**
  * Defines the signature for a function that retrieves JSON Web Key Sets (JWKS) from a client.
@@ -14,57 +11,6 @@ import { NoSuitableKeysFoundInEntityConfiguration } from "./errors";
 export type FetchJwks<T extends Array<unknown> = []> = (...args: T) => Promise<{
   keys: JWK[];
 }>;
-
-/**
- * Retrieves the JSON Web Key Set (JWKS) from the specified client's well-known endpoint.
- * It is formed using `{issUrl.base}/.well-known/jar-issuer${issUrl.pah}` as explained in SD-JWT VC issuer metadata section
- *
- * @param requestObjectEncodedJwt - Request Object in JWT format.
- * @param options - Optional context containing a custom fetch implementation.
- * @param options.context - Optional context object.
- * @param options.context.appFetch - Optional custom fetch function to use instead of the global `fetch`.
- * @returns A promise resolving to an object containing an array of JWKs.
- * @throws Will throw an error if the JWKS retrieval fails.
- */
-export const fetchJwksFromRequestObject: FetchJwks<
-  [string, { context?: { appFetch?: GlobalFetch["fetch"] } }]
-> = async (requestObjectEncodedJwt, { context = {} } = {}) => {
-  const { appFetch = fetch } = context;
-  const requestObjectJwt = decodeJwt(requestObjectEncodedJwt);
-
-  // 1. check if request object jwt contains the 'jwk' attribute
-  if (requestObjectJwt.protectedHeader?.jwk) {
-    return {
-      keys: [JWK.parse(requestObjectJwt.protectedHeader.jwk)],
-    };
-  }
-
-  // 2. According to Potential profile, retrieve from RP endpoint using iss claim
-  const issClaimValue = requestObjectJwt.payload?.iss as string;
-  if (issClaimValue) {
-    const issUrl = new URL(issClaimValue);
-    const wellKnownUrl = new URL(
-      `/.well-known/jar-issuer${issUrl.pathname}`,
-      `${issUrl.protocol}//${issUrl.host}`
-    ).toString();
-
-    // Fetches the JWKS from a specific endpoint of the entity's well-known configuration
-    const jwks = await appFetch(wellKnownUrl, {
-      method: "GET",
-    })
-      .then(hasStatusOrThrow(200))
-      .then((raw) => raw.json())
-      .then((json) => JWKS.parse(json.jwks));
-
-    return {
-      keys: jwks.keys,
-    };
-  }
-
-  throw new NoSuitableKeysFoundInEntityConfiguration(
-    "Request Object signature verification"
-  );
-};
 
 /**
  * Retrieves the JSON Web Key Set (JWKS) from a Relying Party's entity configuration.
