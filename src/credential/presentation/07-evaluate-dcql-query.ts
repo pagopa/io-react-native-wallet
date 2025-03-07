f43665619dd4c204032d78554557762c77f4f262
Gianluca Spada <gianluca.spada@spindox.it>
2025-03-07 11:12:05 +0100

    chore: update evaluateDcqlQuery to improve query evaluation

--- src/credential/presentation/07-evaluate-dcql-query.ts
+++ src/credential/presentation/07-evaluate-dcql-query.ts
@@ -1,8 +1,27 @@
-import { DcqlQuery, DcqlError, DcqlCredentialSetError } from "dcql";
+import {
+  DcqlQuery,
+  DcqlError,
+  DcqlCredentialSetError,
+  DcqlQueryResult,
+} from "dcql";
 import { isValiError } from "valibot";
 import { decode } from "../../sd-jwt";
-import type { Disclosure } from "../../sd-jwt/types";
+import type { Disclosure, DisclosureWithEncoded } from "../../sd-jwt/types";
 import { ValidationFailed } from "../../utils/errors";
+
+type EvaluateDcqlQuery = (
+  credentialsSdJwt: [string /* keyTag */, string /* credential */][],
+  query: DcqlQuery.Input
+) => {
+  requiredDisclosures: DisclosureWithEncoded[];
+  credential: string;
+  keyTag: string;
+}[];
+
+type DcqlMatchSuccess = Extract<
+  DcqlQueryResult.CredentialMatch,
+  { success: true }
+>;
 
 /**
  * Convert a credential in JWT format to an object with claims
@@ -30,11 +49,21 @@ const mapCredentialToObject = (jwt: string) => {
   };
 };
 
-export const evaluateDcqlQuery = (
-  credentialJwts: string[],
-  query: DcqlQuery.Input
+/**
+ * Extract only successful matches from the DCQL query result.
+ */
+const getDcqlQueryMatches = (result: DcqlQueryResult) =>
+  Object.entries(result.credential_matches).filter(
+    ([, match]) => match.success === true
+  ) as [string, DcqlMatchSuccess][];
+
+export const evaluateDcqlQuery: EvaluateDcqlQuery = (
+  credentialsSdJwt,
+  query
 ) => {
-  const credentials = credentialJwts.map(mapCredentialToObject);
+  const credentials = credentialsSdJwt.map(([, credential]) =>
+    mapCredentialToObject(credential)
+  );
 
   try {
     // Validate the query
@@ -44,10 +73,32 @@ export const evaluateDcqlQuery = (
     const queryResult = DcqlQuery.query(parsedQuery, credentials);
 
     if (!queryResult.canBeSatisfied) {
-      // TODO: handle query that cannot be satisfied, e.g. missing claims
+      throw new Error("No credential can satisfy the provided DCQL query");
     }
 
-    return queryResult;
+    // Build an object vct:credentialJwt to map matched credentials to their JWT
+    const credentialsSdJwtByVct = credentials.reduce(
+      (acc, c, i) => ({ ...acc, [c.vct]: credentialsSdJwt[i]! }),
+      {} as Record<string, [string /* keyTag */, string /* credential */]>
+    );
+
+    return getDcqlQueryMatches(queryResult).map(([id, match]) => {
+      if (match.output.credential_format !== "vc+sd-jwt") {
+        throw new Error("Unsupported format"); // TODO [SIW-2082]: support MDOC credentials
+      }
+
+      const [keyTag, credential] = credentialsSdJwtByVct[match.output.vct]!;
+      const requiredDisclosures = Object.values(
+        match.output.claims
+      ) as DisclosureWithEncoded[];
+
+      return {
+        id,
+        keyTag,
+        credential,
+        requiredDisclosures,
+      };
+    });
   } catch (error) {
     // Invalid DCQL query structure
     if (isValiError(error)) {
