46265ecf6e459cf4a5d6d03376fbaf15bff154ec
LazyAfternoons <lazyafternoons@outlook.it>
2025-03-12 17:24:32 +0100

    chore: revert some changes

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,19 +1,22 @@
 const path = require("path");
 const pak = require("../package.json");
 
-module.exports = {
-  presets: ["module:@react-native/babel-preset"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        extensions: [".tsx", ".ts", ".js", ".json"],
-        alias: {
-          [pak.name]: path.join(__dirname, "..", pak.source),
+module.exports = function (api) {
+  api.cache(false); // Required by react-native-dotenv to make babel detect changes in .env file
+  return {
+    presets: ["module:@react-native/babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".tsx", ".ts", ".js", ".json"],
+          alias: {
+            [pak.name]: path.join(__dirname, "..", pak.source),
+          },
         },
-      },
+      ],
+      ["module:react-native-dotenv"],
+      "react-native-reanimated/plugin",
     ],
-    ["module:react-native-dotenv"],
-    "react-native-reanimated/plugin",
-  ],
+  };
 };
