9356afa8140cee28eb54b0120ee8809d603242e2
LazyAfternoons <lazyafternoons@outlook.it>
2025-03-13 11:06:58 +0100

    build: fix Android build

--- example/android/app/src/main/java/it/pagopa/app/io/poc/itwallet/MainApplication.kt
+++ example/android/app/src/main/java/it/pagopa/app/io/poc/itwallet/MainApplication.kt
@@ -10,7 +10,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import it.ipzs.cieidsdk.native_bridge.CiePackage;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -20,7 +19,6 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              add(CiePackage());
             }
 
         override fun getJSMainModuleName(): String = "index"
