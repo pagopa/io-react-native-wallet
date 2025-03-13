64ef9b26dadaf5c5f4ea61798704b505aec05fbb
Gianluca Spada <gianluca.spada@spindox.it>
2025-03-13 15:03:14 +0100

    chore: fix wallet_relying_party

--- src/trust/types.ts
+++ src/trust/types.ts
@@ -186,8 +186,17 @@ export const CredentialIssuerEntityConfiguration = BaseEntityConfiguration.and(
         openid_credential_verifier: RelyingPartyMetadata.optional(),
         /**
          * @deprecated use `openid_credential_verifier`
+         * TODO [SIW-2111]: remove after migrating to 0.9.x
          */
-        wallet_relying_party: RelyingPartyMetadata.optional(), // TODO [SIW-2111]: remove after migrating to 0.9.x
+        wallet_relying_party: z
+          .object({
+            application_type: z.string().optional(),
+            client_id: z.string().optional(),
+            client_name: z.string().optional(),
+            jwks: z.object({ keys: z.array(JWK) }),
+            contacts: z.array(z.string()).optional(),
+          })
+          .optional(),
       }),
     }),
   })
