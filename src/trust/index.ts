a11acc3bef8cb80c53a09e262789040ac9bb2a3b
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2025-06-27 15:25:27 +0200

    feat: add Types export to index for improved type management

--- src/trust/index.ts
+++ src/trust/index.ts
@@ -1,5 +1,6 @@
 import * as Build from "./build-chain";
 import * as Verify from "./verify-chain";
 import * as Errors from "./errors";
+import * as Types from "./types";
 
-export { Build, Verify, Errors };
+export { Build, Verify, Errors, Types };
