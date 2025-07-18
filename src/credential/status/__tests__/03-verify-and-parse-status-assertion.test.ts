9ad479bcb01a7264923694f08627c1e5eee89264
Gianluca Spada <gianluca.spada@spindox.it>
2025-07-18 12:12:38 +0200

    chore: fix tests

--- src/credential/status/__tests__/03-verify-and-parse-status-assertion.test.ts
+++ src/credential/status/__tests__/03-verify-and-parse-status-assertion.test.ts
@@ -3,9 +3,11 @@ import {
   IssuerResponseErrorCodes,
 } from "../../../utils/errors";
 import { verifyAndParseStatusAssertion } from "..";
-import type { CryptoContext } from "@pagopa/io-react-native-jwt";
 import type { EvaluateIssuerTrust } from "../../issuance";
 import type { Out } from "../../../utils/misc";
+
+const mockCredential = "";
+const format = "dc+sd-jwt";
 
 const mockPubKey = {
   kty: "EC",
@@ -16,10 +18,9 @@ const mockPubKey = {
   y: "m38FnFXyd16mqUO6xpz0MrcDiedJFhtLJLENX59nEjI",
 };
 
-const cryptoContext: CryptoContext = {
-  getPublicKey: async () => mockPubKey,
-  getSignature: async () => "",
-};
+jest.mock("../../../utils/credentials", () => ({
+  extractJwkFromCredential: () => mockPubKey,
+}));
 
 const mockIssuerConf = {
   openid_credential_issuer: {
@@ -37,7 +38,8 @@ describe("verifyAndParseStatusAssertion", () => {
         statusAssertion:
           "eyJraWQiOiI3YWM2MzljNy1kZDhhLTRjMzgtOGMwYy00OTVhM2E2NmEwODAiLCJ0eXAiOiJzdGF0dXMtYXNzZXJ0aW9uK2p3dCIsImFsZyI6IkVTMjU2In0.eyJjcmVkZW50aWFsX3N0YXR1c190eXBlIjoiMHgwMCIsImNyZWRlbnRpYWxfaGFzaF9hbGciOiJzaGEtMjU2IiwiaXNzIjoiaHR0cHM6Ly9wcmUuZWFhLndhbGxldC5pcHpzLml0LzEtMCIsImNyZWRlbnRpYWxfaGFzaCI6ImM2YzZmZDhmNjhhZjE5NDBhYzVkYzVhNDljYzAxODg2ZjNmZWEwYzVhYzA3Njk4ZTY1ZGQ5N2IwODVhMmIyMWEiLCJjbmYiOnsiandrIjp7Imt0eSI6IkVDIiwiYWxnIjoiRVMyNTYiLCJraWQiOiI3YWM2MzljNy1kZDhhLTRjMzgtOGMwYy00OTVhM2E2NmEwODAiLCJjcnYiOiJQLTI1NiIsIngiOiJzN1pjdDlKYS1wYUtsSkR1bVo5aUJ3ZGx5UmZEQUpnLUlzaG9TSXJadnVZIiwieSI6Im0zOEZuRlh5ZDE2bXFVTzZ4cHowTXJjRGllZEpGaHRMSkxFTlg1OW5FakkifX0sImV4cCI6MTc1MjgzMjU1NywiaWF0IjoxNzUyNzQ2MTU3LCJqdGkiOiIxNDQ4NTZlOC04MTRhLTRmNjItYmI0NS0xOTZlMjU5ZmFlZTYifQ.9rtsrit-z4YRd3QijW6hKXYCc_YFNNnZPjGtWPzaLFurClQTrBxzABRgfXF9OJ62iDpy7CI5Fr0HlZtkbnT9iA",
       },
-      { credentialCryptoContext: cryptoContext }
+      mockCredential,
+      format
     );
     expect(result.parsedStatusAssertion.payload).toEqual(
       expect.objectContaining({
@@ -59,7 +61,8 @@ describe("verifyAndParseStatusAssertion", () => {
           statusAssertion:
             "eyJraWQiOiI3YWM2MzljNy1kZDhhLTRjMzgtOGMwYy00OTVhM2E2NmEwODAiLCJ0eXAiOiJzdGF0dXMtYXNzZXJ0aW9uK2p3dCIsImFsZyI6IkVTMjU2In0.eyJjcmVkZW50aWFsX3N0YXR1c190eXBlIjoiMHgwMSIsImNyZWRlbnRpYWxfc3RhdHVzX2RldGFpbCI6eyJzdGF0ZSI6InJldm9rZWQiLCJkZXNjcmlwdGlvbiI6IkxvcmVtIGlwc3VtIn0sImNyZWRlbnRpYWxfaGFzaF9hbGciOiJzaGEtMjU2IiwiaXNzIjoiaHR0cHM6Ly9wcmUuZWFhLndhbGxldC5pcHpzLml0LzEtMCIsImNyZWRlbnRpYWxfaGFzaCI6ImM2YzZmZDhmNjhhZjE5NDBhYzVkYzVhNDljYzAxODg2ZjNmZWEwYzVhYzA3Njk4ZTY1ZGQ5N2IwODVhMmIyMWEiLCJjbmYiOnsiandrIjp7Imt0eSI6IkVDIiwiYWxnIjoiRVMyNTYiLCJraWQiOiI3YWM2MzljNy1kZDhhLTRjMzgtOGMwYy00OTVhM2E2NmEwODAiLCJjcnYiOiJQLTI1NiIsIngiOiJzN1pjdDlKYS1wYUtsSkR1bVo5aUJ3ZGx5UmZEQUpnLUlzaG9TSXJadnVZIiwieSI6Im0zOEZuRlh5ZDE2bXFVTzZ4cHowTXJjRGllZEpGaHRMSkxFTlg1OW5FakkifX0sImV4cCI6MTc1MjgzMjU1NywiaWF0IjoxNzUyNzQ2MTU3LCJqdGkiOiIxNDQ4NTZlOC04MTRhLTRmNjItYmI0NS0xOTZlMjU5ZmFlZTYifQ.-b9RC0Gd1gjmInwELJvLwgF3XMFzBrHV669x-oJQO9b1tH3PQN_U6LQ0TBnHlyN9n0MCGJY3n3Sj4vqBKMwL8A",
         },
-        { credentialCryptoContext: cryptoContext }
+        mockCredential,
+        format
       );
     } catch (err) {
       expect(err).toBeInstanceOf(IssuerResponseError);
@@ -84,7 +87,8 @@ describe("verifyAndParseStatusAssertion", () => {
           statusAssertion:
             "eyJraWQiOiI3YWM2MzljNy1kZDhhLTRjMzgtOGMwYy00OTVhM2E2NmEwODAiLCJ0eXAiOiJzdGF0dXMtYXNzZXJ0aW9uLWVycm9yK2p3dCIsImFsZyI6IkVTMjU2In0.eyJjcmVkZW50aWFsX2hhc2hfYWxnIjoic2hhLTI1NiIsImVycm9yX2Rlc2NyaXB0aW9uIjoiQ3JlZGVudGlhbCBub3QgZm91bmQiLCJpc3MiOiJodHRwczovL3ByZS5lYWEud2FsbGV0LmlwenMuaXQvMS0wIiwiY3JlZGVudGlhbF9oYXNoIjoiYjdkY2RhMjc1YjJjNzE3ZGMwMWNiZmU4YmExNjM5MjIwMmJkZGJiOTUwODU2MjIwMzY0NDdhZDA3MzFiMDJmYyIsImV4cCI6MTc1MjgyNTg5OSwiZXJyb3IiOiJjcmVkZW50aWFsX25vdF9mb3VuZCIsImlhdCI6MTc1MjczOTQ5OX0.WanqL5c_JmikZtQugw2YbUaX45mA6NLRUm32Id2W4HBNGKR_Sbt6DbmE3tDijQGyQV_TnjZ1LmKRrxyC1LmYeQ",
         },
-        { credentialCryptoContext: cryptoContext }
+        mockCredential,
+        format
       );
     } catch (err) {
       expect(err).toBeInstanceOf(IssuerResponseError);
