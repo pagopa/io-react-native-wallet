23c3a949174c73e3816f3f201ed216d6e210907d
LazyAfternoons <lazyafternoons@outlook.it>
2025-07-23 17:01:56 +0200

    chore: update react native to version 0.78

--- example/src/utils/idps.ts
+++ example/src/utils/idps.ts
@@ -4,9 +4,8 @@ import { SPID_DEMO_IDPHINT } from "./environment";
 export type Idp = {
   id: string;
   name: string;
-  logo: string;
   profileUrl: string;
-  localLogo?: ImageSourcePropType;
+  localLogo: ImageSourcePropType;
 };
 
 export type IdpList = ReadonlyArray<Idp>;
@@ -19,83 +18,71 @@ export const idps: IdpList = [
     id: "arubaid",
     name: "Aruba ID",
     localLogo: require("../img/spid-idp-arubaid.png"),
-    logo: "",
     profileUrl: "https://selfcarespid.aruba.it",
   },
   {
     id: "ehtid",
     name: "Etna ID",
-    logo: "",
     localLogo: require("../img/spid-idp-etnaid.png"),
     profileUrl: "https://etnaid.eht.eu/",
   },
   {
     id: "infocamereid",
     name: "ID InfoCamere",
-    logo: "",
     localLogo: require("../img/spid-idp-infocamereid.png"),
     profileUrl: "https://selfcarespid.infocamere.it/spid-selfCare/#/login",
   },
   {
     id: "infocertid",
     name: "InfoCert ID",
-    logo: "",
     localLogo: require("../img/spid-idp-infocertid.png"),
     profileUrl: "https://my.infocert.it/selfcare",
   },
   {
     id: "intesiid",
     name: "Intesi Group SPID",
-    logo: "",
     localLogo: require("../img/spid-idp-intesigroupspid.png"),
     profileUrl: "https://spid.intesigroup.com",
   },
   {
     id: "lepidaid",
     name: "Lepida ID",
-    logo: "",
     localLogo: require("../img/spid-idp-lepidaid.png"),
     profileUrl: "https://id.lepida.it/",
   },
   {
     id: "namirialid",
     name: "Namirial ID",
-    logo: "",
     localLogo: require("../img/spid-idp-namirialid.png"),
     profileUrl: "https://idp.namirialtsp.com/idp",
   },
   {
     id: "posteid",
     name: "Poste ID",
-    logo: "",
     localLogo: require("../img/spid-idp-posteid.png"),
     profileUrl: "https://posteid.poste.it/private/cruscotto.shtml",
   },
   {
     id: "sielteid",
     name: "Sielte ID",
-    logo: "",
     localLogo: require("../img/spid-idp-sielteid.png"),
     profileUrl: "https://myid.sieltecloud.it/profile/",
   },
   {
     id: "spiditalia",
     name: "SpidItalia",
-    logo: "",
     localLogo: require("../img/spid-idp-spiditalia.png"),
     profileUrl: "https://spid.register.it",
   },
   {
     id: "timid",
     name: "TIM id",
-    logo: "",
     localLogo: require("../img/spid-idp-timid.png"),
     profileUrl: "https://id.tim.it/identity/private/",
   },
   {
     id: "teamsystemid",
     name: "TeamSystem ID",
-    logo: "",
     localLogo: require("../img/spid-idp-teamsystemid.png"),
     profileUrl: "https://identity.teamsystem.com/",
   },
@@ -105,7 +92,7 @@ export const testIdps: IdpList = [
   {
     id: "spiddemo",
     name: "SPID Demo",
-    logo: "",
+
     localLogo: require("../img/spid.png"),
     profileUrl: SPID_DEMO_IDPHINT,
   },
