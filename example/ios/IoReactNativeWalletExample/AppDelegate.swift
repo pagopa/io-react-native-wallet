9fc110cf377e27df6c0bff5f544c1b5d7168ff08
LazyAfternoons <lazyafternoons@outlook.it>
2025-07-31 10:58:18 +0200

    chore: remove unwanted changes

--- example/ios/IoReactNativeWalletExample/AppDelegate.swift
+++ example/ios/IoReactNativeWalletExample/AppDelegate.swift
@@ -27,9 +27,4 @@ class AppDelegate: RCTAppDelegate {
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
-
-  // Required by react-native-linking for CIE ID authentication
-  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-    return RCTLinkingManager.application(app, open: url, options: options)
-  }
 }
