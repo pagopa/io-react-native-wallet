cde843f0c34d6b0cf5b66256b1f7614cc6ad4dff
Federico Mastrini <federicomastrini93@gmail.com>
2025-08-14 14:03:58 +0200

    chore: remove unused exports

--- example/src/components/cie/index.ts
+++ example/src/components/cie/index.ts
@@ -1,14 +1,3 @@
-import { CiePinDialog } from "./CiePinDialog";
-import { CieAuthenticationWebview } from "./CieAuthenticationWebView";
-import { CieAuthorizationWebview } from "./CieAuthorizationWebView";
 import { useCie } from "./useCie";
 
-import type { CieWebViewError } from "./CieWebView";
-
-export {
-  useCie,
-  CiePinDialog,
-  CieAuthenticationWebview,
-  CieAuthorizationWebview,
-  type CieWebViewError,
-};
+export { useCie };
