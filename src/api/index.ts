f79f536c3a67877ecba3e9d22ffe9a73fab6fcea
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-02 16:12:14 +0100

    chore: index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import * as Trust from "../trust";
+import * as CredentialIssuance from "../credential/issuance";
 import * as CredentialsCatalogue from "../credentials-catalogue";
 
 export type ItwVersion = "1.0.0" | "1.3.3";
@@ -9,6 +10,7 @@ export type ItwVersion = "1.0.0" | "1.3.3";
 export interface IoWalletApi {
   Trust: Trust.TrustApi;
   CredentialsCatalogue: CredentialsCatalogue.CredentialsCatalogueApi;
+  CredentialIssuance: CredentialIssuance.IssuanceApi;
 }
 
 /**
@@ -17,10 +19,13 @@ export interface IoWalletApi {
 export const ioWalletApiByVersion: Record<ItwVersion, IoWalletApi> = {
   "1.0.0": {
     Trust: Trust.V1_0_0,
+    CredentialIssuance: CredentialIssuance.V1_0_0,
     CredentialsCatalogue: CredentialsCatalogue.V1_0_0,
   },
+  // TODO: replace v1.0.0 with v1.3.3 implementations
   "1.3.3": {
     Trust: Trust.V1_0_0,
+    CredentialIssuance: CredentialIssuance.V1_0_0,
     CredentialsCatalogue: CredentialsCatalogue.V1_3_3,
   },
 };
