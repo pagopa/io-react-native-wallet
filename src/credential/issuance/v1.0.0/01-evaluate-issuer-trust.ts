d1e44fedd49b13c29cd6a8a736a754ac781d2e0d
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-02 15:42:11 +0100

    chore: evaluateIssuerTrust 1.0.0

--- src/credential/issuance/v1.0.0/01-evaluate-issuer-trust.ts
+++ src/credential/issuance/v1.0.0/01-evaluate-issuer-trust.ts
@@ -1,33 +1,12 @@
-import { CredentialIssuerEntityConfiguration } from "../../trust/v1.0.0/types"; // TODO: [SIW-3741] refactor issuance
-import type { StartFlow } from "./01-start-flow";
-import type { Out } from "../../utils/misc";
-import { getCredentialIssuerEntityConfiguration } from "../../trust/v1.0.0/entities"; // TODO: [SIW-3741] refactor issuance
+import { getCredentialIssuerEntityConfiguration } from "../../../trust/v1.0.0/entities";
+import type { EvaluateIssuerTrustApi } from "../api/01-evaluate-issuer-trust";
+import { mapToIssuerConfig } from "./mappers";
 
-export type EvaluateIssuerTrust = (
-  issuerUrl: Out<StartFlow>["issuerUrl"],
-  context?: {
-    appFetch?: GlobalFetch["fetch"];
-  }
-) => Promise<{
-  issuerConf: CredentialIssuerEntityConfiguration["payload"]["metadata"];
-}>;
+export const evaluateIssuerTrust: EvaluateIssuerTrustApi["evaluateIssuerTrust"] =
+  async (issuerUrl, context = {}) => {
+    const issuerConf = await getCredentialIssuerEntityConfiguration(issuerUrl, {
+      appFetch: context.appFetch,
+    });
 
-/**
- * WARNING: This function must be called after {@link startFlow}. The next function to be called is {@link startUserAuthorization}.
- * The Issuer trust evaluation phase.
- * Fetch the Issuer's configuration and verify trust.
- *
- * @param issuerUrl The base url of the Issuer returned by {@link startFlow}
- * @param context.appFetch (optional) fetch api implementation. Default: built-in fetch
- * @returns The Issuer's configuration
- */
-export const evaluateIssuerTrust: EvaluateIssuerTrust = async (
-  issuerUrl,
-  context = {}
-) => {
-  const issuerConf = await getCredentialIssuerEntityConfiguration(issuerUrl, {
-    appFetch: context.appFetch,
-  }).then(({ payload }) => payload.metadata);
-
-  return { issuerConf };
-};
+    return { issuerConf: mapToIssuerConfig(issuerConf) };
+  };
---
+++ src/credential/issuance/v1.0.0/mappers.ts
@@ -0,0 +1,25 @@
+import { CredentialIssuerEntityConfiguration } from "../../../trust/v1.0.0/types";
+import { createMapper } from "../../../utils/mappers";
+import type { IssuerConfig } from "../api/IssuerConfig";
+
+export const mapToIssuerConfig = createMapper<
+  CredentialIssuerEntityConfiguration,
+  IssuerConfig
+>((x) => {
+  const { oauth_authorization_server, openid_credential_issuer } =
+    x.payload.metadata;
+  return {
+    authorization_endpoint: oauth_authorization_server.authorization_endpoint,
+    credential_endpoint: openid_credential_issuer.credential_endpoint,
+    credential_issuer: openid_credential_issuer.credential_issuer,
+    credential_configurations_supported:
+      openid_credential_issuer.credential_configurations_supported,
+    keys: openid_credential_issuer.jwks.keys,
+    pushed_authorization_request_endpoint:
+      oauth_authorization_server.pushed_authorization_request_endpoint,
+    token_endpoint: oauth_authorization_server.token_endpoint,
+    status_assertion_endpoint:
+      openid_credential_issuer.status_attestation_endpoint,
+    nonce_endpoint: openid_credential_issuer.nonce_endpoint,
+  };
+});
