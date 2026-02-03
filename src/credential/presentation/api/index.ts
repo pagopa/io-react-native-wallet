2a97ff5cd6934dce3db1414c604eb501522501a1
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-03 16:46:25 +0100

    chore: add RetrieveRpJwksApi

--- src/credential/presentation/api/index.ts
+++ src/credential/presentation/api/index.ts
@@ -1,14 +1,16 @@
 import type { StartFlowApi } from "./01-start-flow";
 import type { EvaluateRelyingPartyTrustApi } from "./02-evaluate-rp-trust";
 import type { GetRequestObjectApi } from "./03-get-request-object";
-import type { EvaluateDcqlQueryApi } from "./04-evaluate-dcql-query";
-import type { SendAuthorizationResponseApi } from "./05-send-authorization-response";
+import type { RetrieveRpJwksApi } from "./04-retrieve-rp-jwks";
+import type { EvaluateDcqlQueryApi } from "./05-evaluate-dcql-query";
+import type { SendAuthorizationResponseApi } from "./06-send-authorization-response";
 import type { RelyingPartyConfig } from "./RelyingPartyConfig";
 
 export interface RemotePresentationApi
   extends StartFlowApi,
     EvaluateRelyingPartyTrustApi,
     GetRequestObjectApi,
+    RetrieveRpJwksApi,
     EvaluateDcqlQueryApi,
     SendAuthorizationResponseApi {}
 
