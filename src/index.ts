8d37af4b3329f06ce0ad73a7e17f5c153a82b745
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2026-02-09 16:45:06 +0100

    feat: add CredentialOffer module to index exports

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import "react-native-url-polyfill/auto";
 import * as Trust from "./trust";
 import * as CredentialsCatalogue from "./credentials-catalogue";
 import * as CredentialIssuance from "./credential/issuance";
+import * as CredentialOffer from "./credential/offer";
 import * as RemotePresentation from "./credential/presentation";
 import * as Credential from "./credential";
 import * as PID from "./pid";
@@ -23,6 +24,7 @@ import type { IntegrityContext } from "./utils/integrity";
 export {
   Trust,
   CredentialIssuance,
+  CredentialOffer,
   CredentialsCatalogue,
   RemotePresentation,
   SdJwt,
