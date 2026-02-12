2547c97338e06cdda3e5ce34078ed01499cf915f
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-12 17:24:41 +0100

    chore: update issuance index 1.3

--- src/credential/issuance/v1.3.3/index.ts
+++ src/credential/issuance/v1.3.3/index.ts
@@ -1,8 +1,22 @@
 import type { IssuanceApi } from "../api";
 import { evaluateIssuerTrust } from "./01-evaluate-issuer-trust";
-import { startUserAuthorization } from "./02-start-user-authorization";;
+import { startUserAuthorization } from "./02-start-user-authorization";
+import {
+  continueUserAuthorizationWithMRTDPoPChallenge,
+  completeUserAuthorizationWithQueryMode,
+  completeUserAuthorizationWithFormPostJwtMode,
+  buildAuthorizationUrl,
+  getRequestedCredentialToBePresented,
+} from "./03-complete-user-authorization";
+import { authorizeAccess } from "./04-authorize-access";
 
 export const Issuance: IssuanceApi = {
   evaluateIssuerTrust,
   startUserAuthorization,
+  buildAuthorizationUrl,
+  completeUserAuthorizationWithQueryMode,
+  continueUserAuthorizationWithMRTDPoPChallenge,
+  getRequestedCredentialToBePresented,
+  completeUserAuthorizationWithFormPostJwtMode,
+  authorizeAccess,
 };
