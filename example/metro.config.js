9071a87c589fc558c36e958140f2f2418f8cc750
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-20 14:30:21 +0100

    chore: fix Metro resolution issues

--- example/metro.config.js
+++ example/metro.config.js
@@ -41,6 +41,8 @@ const config = {
       }
       return context.resolveRequest(context, moduleName, platform);
     },
+
+    resolverMainFields: ["react-native", "browser", "main", "module"],
   },
 
   transformer: {
