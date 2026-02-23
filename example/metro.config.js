bd90be9d3cd57f90be05bf30ca161ff67b256cab
Gianluca Spada <gianluca.spada@spindox.it>
2026-02-23 09:49:27 +0100

    chore: add comment in Metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -42,7 +42,12 @@ const config = {
       return context.resolveRequest(context, moduleName, platform);
     },
 
-    resolverMainFields: ["react-native", "browser", "main", "module"],
+    resolverMainFields: [
+      "react-native",
+      "browser",
+      "main",
+      "module", // Some packages (ex. @openid4vc) do not expose a main field and Metro defaults to the wrong file
+    ],
   },
 
   transformer: {
