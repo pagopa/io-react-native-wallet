f85e040378305504ee23f8e757e09cb7013bed3e
MonicaRungi-EY <Monica.Rungi@it.ey.com>
2026-04-01 15:09:27 +0200

    fix import

--- src/credential/presentation/v1.3.3/04-verify-certificate-chain.ts
+++ src/credential/presentation/v1.3.3/04-verify-certificate-chain.ts
@@ -7,8 +7,8 @@ import {
 import {
   MissingX509CertsError,
   X509ValidationError,
-} from "src/trust/common/errors";
-import { Logger, LogLevel } from "src/utils/logging";
+} from "../../../trust/common/errors";
+import { Logger, LogLevel } from "../../../utils/logging";
 
 export const verifyAuthRequestCertificateChain: RemotePresentationApi["verifyAuthRequestCertificateChain"] =
   async (requestObjectJwt, { caRootCerts }) => {
@@ -24,24 +24,24 @@ export const verifyAuthRequestCertificateChain: RemotePresentationApi["verifyAut
 
     if (!certChain) {
       throw new MissingX509CertsError(
-        "No certificate chain (x5c) found in the Request Object"
+        "No certificate chain (x5c) found in the Request Object",
       );
     }
 
     const validationResult = await verifyCertificateChain(
       certChain,
       caRootCerts,
-      x509Options
+      x509Options,
     );
 
     if (!validationResult.isValid) {
       Logger.log(
         LogLevel.ERROR,
-        `Certificate chain failure: ${validationResult.validationStatus} - ${validationResult.errorMessage}`
+        `Certificate chain failure: ${validationResult.validationStatus} - ${validationResult.errorMessage}`,
       );
 
       throw new X509ValidationError(
-        "X.509 certificate chain validation failed"
+        "X.509 certificate chain validation failed",
       );
     }
 
