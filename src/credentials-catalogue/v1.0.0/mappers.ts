bb5f8c44a29832d22e2ece0edeac003ddb4f0b0f
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2026-04-10 15:01:04 +0200

    fix: [SIW-4130] Improve credential catalogue schema (#347)

--- src/credentials-catalogue/v1.0.0/mappers.ts
+++ src/credentials-catalogue/v1.0.0/mappers.ts
@@ -1,18 +1,33 @@
 import { createMapper } from "../../utils/mappers";
-import { DigitalCredentialsCatalogue } from "../api/DigitalCredentialsCatalogue";
-import { DigitalCredentialsCatalogueJwt } from "./types";
+import {
+  DigitalCredentialsCatalogue,
+  type DigitalCredentialsCatalogue as DigitalCredentialsCatalogueType,
+} from "../api/DigitalCredentialsCatalogue";
+import {
+  DigitalCredentialsCatalogueJwt,
+  type DigitalCredentialsCatalogueJwt as DigitalCredentialsCatalogueJwtType,
+} from "./types";
 
-export const mapToCredentialsCatalogue = createMapper(
-  ({ payload }) => ({
-    ...payload,
-    credentials: payload.credentials.map((credential) => ({
-      ...credential,
-      authentic_sources: credential.authentic_sources.map((as) => ({
-        ...as,
-        organization_type: as.source_type,
+export const mapToCredentialsCatalogue = createMapper<
+  DigitalCredentialsCatalogueJwtType,
+  DigitalCredentialsCatalogueType
+>(
+  ({ payload }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { catalog_version, ...rest } = payload;
+    return {
+      ...rest,
+      credentials: payload.credentials.map((credential) => ({
+        ...credential,
+        authentic_sources: credential.authentic_sources.map(
+          ({ source_type, ...as }) => ({
+            ...as,
+            organization_type: source_type,
+          })
+        ),
       })),
-    })),
-  }),
+    };
+  },
   {
     inputSchema: DigitalCredentialsCatalogueJwt,
     outputSchema: DigitalCredentialsCatalogue,
