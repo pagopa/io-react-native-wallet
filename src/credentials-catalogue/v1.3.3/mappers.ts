c79cd379ff8674dd9c3ae51cb37bb05102d93fbb
Gianluca Spada <gianluca.spada@spindox.it>
2026-08-26 12:44:53 +0200

    chore: lint

--- src/credentials-catalogue/v1.3.3/mappers.ts
+++ src/credentials-catalogue/v1.3.3/mappers.ts
@@ -55,8 +55,8 @@ export const mapToCredentialsCatalogue = createMapper<
         (dc) => dc.dataset_id === dataset_id,
       );
       return {
-        id,
         data_origin_l10n_id: dataCapability?.data_origin_l10n_id,
+        id,
         organization_code: ipa_code,
         organization_name_l10n_id,
         user_information_l10n_id: dataCapability?.user_information_l10n_id,
